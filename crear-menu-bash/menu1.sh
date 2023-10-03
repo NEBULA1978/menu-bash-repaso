6d0870af0d26a5da0cc58d7485aafb5366d6f5d8
NEBULA1978 <rpascualey@gmail.com>
2023-10-03 17:12:10 +0200

    Paso inicial 5 creando menu bash crear menu

--- crear-menu-bash/menu1.sh
+++ crear-menu-bash/menu1.sh
@@ -1,26 +1,17 @@
 #!/bin/bash
 
-# Lee el contenido del archivo "texto1.txt" y lo almacena en un arreglo de opciones.
-mapfile -t texto_opciones < texto1.txt
-
-# Definimos un array con las opciones del menú. Cada opción es una cadena con dos partes
-# separadas por el carácter '|': el nombre de la opción y el comando correspondiente.
+# Definimos un array con las opciones del menú. Cada opción es una cadena con dos partes separadas por el carácter '|': el nombre de la opción y el comando correspondiente.
 opciones=(
     "Mostrar archivos del directorio actual:|ls"
     "Mostrar calendario:|cal"
-    "Mostrar día de hoy:|date"
+    "Mostrar dia de hoy:|date"
     "Ver wifi conectado en linux mint:|nmcli device wifi list"
+    "Convertir un formato de fichero texto desde MSDOS a UNIX:|dos2unix filedos.txt fileunix.txt"
+    "Convertir un formato de fichero de texto desde UNIX a MSDOS:|unix2dos fileunix.txt filedos.txt"
+    "Convertir un fichero de texto en HTML:|recode ..HTML < page.txt > page.html"
+    "Mostrar todas las conversiones de formato disponibles:|recode -l | more"
     "Salir:|exit 0"
 )
-
-# Calculamos el número de línea inicial para las opciones adicionales.
-numero_linea_inicial=$(( ${#opciones[@]} ))
-
-# Agregamos las opciones del archivo "texto1.txt" al arreglo de opciones del menú.
-for ((i=0; i<${#texto_opciones[@]}; i++)); do
-    numero_linea=$(( numero_linea_inicial + i ))
-    opciones+=("$numero_linea. ${texto_opciones[i]}") # Agregamos el número de línea.
-done
 
 while true; do
     clear
@@ -28,7 +19,7 @@ while true; do
     echo "==============================="
     echo "Escoja una opción:"
     for ((i=0; i<${#opciones[@]}; i++)); do
-        echo "${opciones[i]%%:*}" # Imprimimos el nombre de la opción (sin el comando).
+        echo "$i. ${opciones[i]%%:*}" # Imprimimos el índice y el nombre de la opción (sin el comando).
     done
     echo "==============================="
     read -p "Ingrese el número de la opción: " opcion
